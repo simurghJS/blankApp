5c99f0765c0668442c7cc9143533768ac3832bb3
siyamak <siabeheshti@gmail.com>
2021-03-08 00:46:22 +0330

    update core

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 import * as simurgh from 'simurgh/app/application';
-import { CreateStackNavigation } from 'simurgh/app/navigation';
 
 let myApp = new simurgh.Application();
 
-let mystack = new CreateStackNavigation();
-mystack.add('/', () => { return (<h1>hello world</h1>); });
-mystack.add('/test', () => { return (<h1>hello world test</h1>); });
-myApp.navigation = mystack;
+/** configure your application */
 
 myApp.build();
